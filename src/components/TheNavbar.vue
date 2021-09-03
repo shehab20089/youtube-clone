3467d8c7b39cbbfe887ac10a58cdafa74d0652ca
shehab20089 <xxxegygamerxxx@gmail.com>
2021-09-03 14:53:48 +0200

    adding the mobile view for the nav bar

---
+++ src/components/TheNavbar.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="navbar">
+    <div class="navbar__avatar">
+      <img src="../assets/whiteLogo.png" alt="" srcset="" />
+    </div>
+    <div class="navbar__searchbar">
+      <TheNavbarSearchBar />
+    </div>
+  </div>
+</template>
+
+<script>
+import TheNavbarSearchBar from "./TheNavbarSearchBar.vue";
+export default {
+  components: {
+    TheNavbarSearchBar,
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+@import "../styles/components/TheNavbar";
+</style>
---
+++ src/components/TheNavbarSearchBar.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="searchbar">
+    <i v-if="SearchIconVisable" class="fa fa-search fa-lg"></i>
+
+    <input
+      type="search"
+      @blur="SearchIconVisable = true"
+      @focus="SearchIconVisable = false"
+      v-model="searchKey"
+      @search="searchVideo"
+    />
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      SearchIconVisable: true,
+      searchKey: "",
+    };
+  },
+
+  methods: {
+    searchVideo() {
+      //   TODO Search Video
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+@import "../styles/components/TheNavbarSearchBar";
+</style>
