b3cc90a83f6a9b412aa4829c876a63e4cdb00846
shehab20089 <xxxegygamerxxx@gmail.com>
2021-09-03 21:09:16 +0200

    breaking videoItem to multiple  components

--- src/components/ListVideoItem.vue
+++ src/components/ListVideoItem.vue
@@ -1,23 +1,18 @@
 <template>
   <div class="listItem">
     <ListVideoItemShot />
-    <div class="listItem__videoData">
-      <div class="videoData__title">
-        Spongebob in Real Life Episode 2 dqjdoiqjdoiqjdqioj qoidjqiodjqoij dqd
-        qdqdq qd qd qd qq dqjdoiqjdoiqjdqiojdq fq qd qdqd qdq qdqdqd
-        qdojqiodjqoijd qoidjqiodjqd
-      </div>
-      <div class="videoData__chName small-text">SpongebobMovid</div>
-      <div class="videoData__noOfViews small-text">54,0400,4000 views</div>
-    </div>
+    <ListVideoItemData />
   </div>
 </template>
 
 <script>
 import ListVideoItemShot from "./ListVideoItemShot.vue";
+import ListVideoItemData from "./ListVideoItemData.vue";
+
 export default {
   components: {
     ListVideoItemShot,
+    ListVideoItemData,
   },
 };
 </script>
---
+++ src/components/ListVideoItemData.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="listItem__videoData">
+    <div class="videoData__title">
+      Spongebob in Real Life Episode 2 dqjdoiqjdoiqjdqioj qoidjqiodjqoij dqd
+      qdqdq qd qd qd qq dqjdoiqjdoiqjdqiojdq fq qd qdqd qdq qdqdqd
+      qdojqiodjqoijd qoidjqiodjqd
+    </div>
+    <div class="videoData__chName small-text">SpongebobMovid</div>
+    <div class="videoData__noOfViews small-text">54,0400,4000 views</div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped>
+@import "../styles/components/ListVideoItemData";
+</style>
