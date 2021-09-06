7066521a6d6b160a99b626e34e79f3c98ad525a4
shehab20089 <xxxegygamerxxx@gmail.com>
2021-09-06 16:22:56 +0200

    adding Video Details card component

---
+++ src/components/VideoDetailsCard.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="videoDetails">
+    <div class="videoDetails__title">
+      <div>SpongPop SquarePants In Real Life Episode 2</div>
+      <i class="fa fa-caret-down descIcon"></i>
+    </div>
+    <div class="videoDetails__chName">
+      JChaseFilms - <span class="small-text">54,056,132 views</span>
+    </div>
+    <div class="videoDetails__actions">
+      <div class="action__interaction">
+        <i class="fa fa-thumbs-o-up"></i><span class="small-text"> 194k</span>
+        <i class="fa fa-thumbs-o-down"></i> <span class="small-text"> 25k</span>
+      </div>
+      <div class="actions__share">
+        <i class="fa fa-plus"></i>
+        <i class="fa fa-rotate-right"></i>
+        <i class="fa fa-flag"></i>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped>
+@import "../styles/components/videoDetailsCard";
+</style>
